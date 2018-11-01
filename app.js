e9827c88d300d9cd2a9db17fe6ac66a079033987
romarios1987 <romios047@gmail.com>
2018-11-01 22:12:19 +0200

    create layout, folder - public(static)

--- app.js
+++ app.js
@@ -1,40 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const Post = require('./models/Post');
-
+const path = require('path');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    '/javascript',
+    express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist'))
+);
 
 app.get('/', (req, res) => {
-    Post.find({}).then(posts => {
-        res.render('index', {posts: posts})
-    })
-});
-
-
-
-app.get('/create', (req, res) => res.render('create'));
-app.post('/create', (req, res) => {
-    // arr.push(req.body.text);
-
-
-    // Реструктуризация
-    const {title, body} = req.body;
-    Post.create({
-        title: title,
-        body: body
-    }).then(post => console.log(post._id));
-
-    res.redirect('/');
+    res.render('index')
 });
 
 module.exports = app;

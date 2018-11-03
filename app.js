87fa5079a807d77eca342514318a249a4bf31693
romarios1987 <romios047@gmail.com>
2018-11-03 22:15:27 +0200

    static-asset(caching static)

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const staticAsset = require('static-asset');
 const path = require('path');
 
+
+// express
 const app = express();
 
+// set
 app.set('view engine', 'ejs');
 
+
+// use
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(staticAsset(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/javascript', express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist')));
 app.use('/javascript', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'js')));
 
 
-
-
-
+// routes
 app.get('/', (req, res) => {
     res.render('index')
 });

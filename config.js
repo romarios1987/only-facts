9df90e99e10c4e904f76781fdfd331b8a6e2c202
romarios1987 <romios047@gmail.com>
2018-11-08 23:20:30 +0200

    config nodemon & add .env

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
+const dotenv = require('dotenv');
+const path = require('path');
+const root = path.join.bind(this, __dirname);
+dotenv.config({path: root('.env')});
+
 module.exports = {
-    PORT: 3000,
-    mongoURI: 'mongodb://Remi:remi111@ds147033.mlab.com:47033/facts',
+    PORT: process.env.PORT || 3000,
+    mongoURI: process.env.MONGO_URL,
     IS_PRODUCTION: process.env.NODE_ENV === 'production'
 };
